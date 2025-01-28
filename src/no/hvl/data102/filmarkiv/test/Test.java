df4466c8c8df0b45516fdaa2b94239f79888eb26
186500-hvl <eirikinnselset@hotmail.com>
2025-01-28 17:47:56 +0100

    hm

---
+++ src/no/hvl/data102/filmarkiv/test/Test.java
@@ -0,0 +1,12 @@
+package no.hvl.data102.filmarkiv.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
