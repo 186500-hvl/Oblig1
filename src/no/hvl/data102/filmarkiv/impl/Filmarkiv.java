7bdfa2b4ec8b5d5a7c5a803f4d69385d09216acb
186500-hvl <eirikinnselset@hotmail.com>
2025-01-29 15:48:08 +0100

    hm3

---
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -0,0 +1,107 @@
+package no.hvl.data102.filmarkiv.impl;
+
+import no.hvl.data102.filmarkiv.adt.Film;
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+import no.hvl.data102.filmarkiv.adt.Sjanger;
+
+public class Filmarkiv {
+	public class Filmarkiv implements FilmarkivADT {
+
+	    private Film[] filmer;
+	    private int antall;
+
+	    public Filmarkiv(int kapasitet) {
+	        filmer = new Film[kapasitet];
+	        antall = 0;
+	    }
+
+	    @Override
+	    public Film finnFilm(int nr) {
+	        for (int i = 0; i < antall; i++) {
+	            if (filmer[i].getFilmNr() == nr) {
+	                return filmer[i];
+	            }
+	        }
+	        return null;
+	    }
+
+	    @Override
+	    public void leggTilFilm(Film nyFilm) {
+	        if (antall == filmer.length) {
+	            utvid();
+	        }
+	        filmer[antall] = nyFilm;
+	        antall++;
+	    }
+
+	    @Override
+	    public boolean slettFilm(int filmnr) {
+	        for (int i = 0; i < antall; i++) {
+	            if (filmer[i].getFilmNr() == filmnr) {
+	                // Flytt siste film til den slettede filmens plass
+	                filmer[i] = filmer[antall - 1];
+	                filmer[antall - 1] = null; 
+	                antall--;
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+
+	    @Override
+	    public Film[] soekTittel(String delstreng) {
+	        Film[] resultat = new Film[antall];
+	        int teller = 0;
+	        for (int i = 0; i < antall; i++) {
+	            if (filmer[i].getTittel().toLowerCase().contains(delstreng.toLowerCase())) {
+	                resultat[teller] = filmer[i];
+	                teller++;
+	            }
+	        }
+	        return trimTabell(resultat, teller);
+	    }
+
+	    @Override
+	    public Film[] soekProdusent(String delstreng) {
+	        Film[] resultat = new Film[antall];
+	        int teller = 0;
+	        for (int i = 0; i < antall; i++) {
+	            if (filmer[i].getProdusent().toLowerCase().contains(delstreng.toLowerCase())) {
+	                resultat[teller] = filmer[i];
+	                teller++;
+	            }
+	        }
+	        return trimTabell(resultat, teller);
+	    }
+
+	    @Override
+	    public int antall(Sjanger sjanger) {
+	        int antallSjanger = 0;
+	        for (int i = 0; i < antall; i++) {
+	            if (filmer[i].getSjanger() == sjanger) {
+	                antallSjanger++;
+	            }
+	        }
+	        return antallSjanger;
+	    }
+
+	    @Override
+	    public int antall() {
+	        return antall;
+	    }
+
+	    private void utvid() {
+	        Film[] nyTabell = new Film[filmer.length * 2];
+	        System.arraycopy(filmer, 0, nyTabell, 0, filmer.length);
+	        filmer = nyTabell;
+	    }
+
+	    // Hjelpemetode for å trimme tabeller
+	    private Film[] trimTabell(Film[] tabell, int antallElementer) {
+	        Film[] trimmetTabell = new Film[antallElementer];
+	        System.arraycopy(tabell, 0, trimmetTabell, 0, antallElementer);
+	        return trimmetTabell;
+	    }
+	}
+}
+}
--- src/no/hvl/data102/filmarkiv/klient/Meny.java
+++ src/no/hvl/data102/filmarkiv/klient/Meny.java
@@ -1,5 +1,71 @@
 package no.hvl.data102.filmarkiv.klient;
 
+import java.util.Scanner;
+
+import no.hvl.data102.filmarkiv.adt.Filmarkiv;
+import no.hvl.data102.filmarkiv.adt.Film;
+import no.hvl.data102.filmarkiv.adt.Sjanger;
+
 public class Meny {
 
+    private Filmarkiv filmarkiv;
+    private Tekstgrensesnitt tekstgrensesnitt;
+
+    public Meny(Filmarkiv filmarkiv) {
+        this.filmarkiv = filmarkiv;
+        this.tekstgrensesnitt = new Tekstgrensesnitt();
+    }
+
+    public void start() {
+        // Legg til noen filmer (hardkodet eller fra fil)
+        // Eksempel på hardkoding av filmer:
+        filmarkiv.leggTilFilm(new Film(1, "The Shawshank Redemption", "Frank Darabont", 1994, Sjanger.DRAMA, "Columbia Pictures"));
+        filmarkiv.leggTilFilm(new Film(2, "The Dark Knight", "Christopher Nolan", 2008, Sjanger.ACTION, "Warner Bros."));
+        // ... legg til flere filmer ...
+
+        Scanner scanner = new Scanner(System.in);
+        int valg;
+
+        do {
+            visMeny();
+            valg = scanner.nextInt();
+            scanner.nextLine(); // For å fjerne newline
+
+            switch (valg) {
+                case 1:
+                    tekstgrensesnitt.visAlleFilmer(filmarkiv);
+                    break;
+                case 2:
+                    tekstgrensesnitt.leggTilFilm(filmarkiv);
+                    break;
+                case 3:
+                    tekstgrensesnitt.slettFilm(filmarkiv);
+                    break;
+                case 4:
+                    tekstgrensesnitt.soekEtterTittel(filmarkiv);
+                    break;
+                case 5:
+                    tekstgrensesnitt.soekEtterProdusent(filmarkiv);
+                    break;
+                case 0:
+                    System.out.println("Avslutter programmet...");
+                    break;
+                default:
+                    System.out.println("Ugyldig valg!");
+            }
+        } while (valg != 0);
+
+        scanner.close();
+    }
+
+    private void visMeny() {
+        System.out.println("\nFilmarkivmeny:");
+        System.out.println("1. Vis alle filmer");
+        System.out.println("2. Legg til film");
+        System.out.println("3. Slett film");
+        System.out.println("4. Søk etter tittel");
+        System.out.println("5. Søk etter produsent");
+        System.out.println("0. Avslutt");
+        System.out.print("Valg: ");
+    }
 }
